0cf81139bc158048b2012b29714bf2e60895f1d1
danielolasupo02 <danielolasupo02@gmail.com>
2024-02-08 16:48:48 +0100

    add complete practice for single-linked list

--- 3. Linked Lists/linked-list.js
+++ 3. Linked Lists/linked-list.js
@@ -165,18 +165,100 @@ class LinkedList {
   }
 
   append(value) {
-    const newNode = {
-      value: value,
-      next: null,
-    };
+    const newNode = new Node(value);
 
     this.tail.next = newNode; // this.head.next = newNode
-    this.tail = newNode;
+    this.tail = newNode; //this.head = newNode
     this.length++;
   }
-}
 
+  prepend(value) {
+    const newNode = new Node(value);
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+  }
+
+  // length => 3
+  // index => 3
+  // whenever index is greater than or equal to object length, append
+
+  //   insert(index, value) {
+  //     //check params
+  //     if (index >= this.length) {
+  //       return this.append(value);
+  //     }
+  //     const newNode = new Node(value);
+  //     const leader = this.traverseToIndex(index - 1);
+  //     const holdingPointer = leader.next;
+  //     leader.next = newNode;
+  //     newNode.next = holdingPointer;
+  //     this.length++;
+  //     return this.printList();
+  //   }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    let newNode = new Node(value);
+    let leader = this.traverseToIndex(index - 1);
+    let holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+
+  remove(index) {
+    // check params
+    if (index >= this.length) {
+      return this;
+    }
+    //10 - 5 - 6
+    let leader = this.traverseToIndex(index - 1);
+    let unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.printList();
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(array);
+  }
+
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+}
+// 10 - 5 - 16
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
+// // 2 - 10 - 5 - 16
+// myLinkedList.prepend(2);
+
+// myLinkedList.printList();
+// myLinkedList.traverseToIndex(2);
+// // console.log(myLinkedList);
+// myLinkedList.insert(2, 70);
+// myLinkedList.insert(3, 45);
+myLinkedList.remove(2);
+// myLinkedList.remove(0);
+
+myLinkedList.printList();
 console.log(myLinkedList);
